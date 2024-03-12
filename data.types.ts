dd3c1b66a1edfa4f00f18f05bd831bd192007c8f
Muskan Nisar <159582953+muskann12@users.noreply.github.com>
2024-03-12 14:38:56 -0700

    Add files via upload

---
+++ data.types.ts
@@ -0,0 +1,25 @@
+//string
+let message :string ="hello its me muskan here"
+console.log(message)
+
+//number
+let numA :number = 20
+let numB :number = 50
+console.log(numA + numB)
+
+//boolean
+let isPaid :boolean = false
+console.log(isPaid)
+
+//Bigint
+let x :bigint = 92345897065740n
+let y :bigint = 901235547088640n
+let sum :bigint = x+y
+console.log("sum of x and y", sum)
+
+
+//null
+let or = null
+console.log(or)
+
+
