58aaac28b886cea6c61f7b282966a207587bacc6
Muskan Nisar <159582953+muskann12@users.noreply.github.com>
2024-03-13 08:36:15 -0700

    Add files via upload

---
+++ object.ts
@@ -0,0 +1,24 @@
+//Objects
+
+//object type
+type Objecttype = {Name:string,Gender:string,Age:number,Email:string}
+
+
+let employes: Objecttype = {
+    Name : "Muskan",
+    Gender : "female",
+    Age : 18,
+    Email : "nisarmuskan14@gmail.com",
+
+}
+
+let employe2 : Objecttype ={
+    Name : "Ahmed",
+    Gender : "Male",
+    Age : 20,
+    Email : "ahmedraza@gmail.com",
+
+}
+//now we will print this.
+console.log(employes.Age);
+console.log(employe2.Gender);
